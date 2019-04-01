0bfc69272f6e2210cbe0db59cb68e7e0b98ead4e
HILLARY-oti <hillaryotieno646@gmail.com>
2019-04-01 11:00:22 +0300

    add variables

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+  function check() {
+      var questionOne = document.Quiz.questionOne.value;
+      var questionTwo = document.Quiz.questionTwo.value;
+      var questionThree = document.Quiz.questionThree.value;
+      var right= 0;
+
+
+       if (questionOne == "correct") {
+           right = right + 10;
+       };
+       if (questionTwo == "correct") {
+           right = right + 10;
+       };
+       if (questionThree == "correct") {
+           right = right + 10;
+       };
+
+
+       document.getElementById("fifth-p").style.visibility = "visible";
+       document.getElementById("correctNumber").innerHTML = "HERE IS YOUR SCORE: " + right + "%";
+       $(".container").fadeOut();
+
+       };
